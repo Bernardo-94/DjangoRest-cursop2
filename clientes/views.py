ab6d7cc6a2fbe5c639b5793f9c44d80be9c23199
Bernardo Cardoso <thecardosobernardo@gmail.com>
2021-03-04 16:27:43 -0300

    Fixing a bug on views.py

--- clientes/views.py
+++ clientes/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.authentication import BaseAuthentication
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class ClientesViewSet(viewsets.ModelViewSet):
@@ -13,5 +13,5 @@ class ClientesViewSet(viewsets.ModelViewSet):
     ordering_fields = ['nome']
     search_fields = ['nome', 'email', 'cpf', 'rg']
     filterset_fields = ['ativo']
-    authentication_classes = [BaseAuthentication]
+    authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
